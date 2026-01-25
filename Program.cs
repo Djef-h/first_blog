8a5d038a6ef3c00c6154062acc6c83acc4a29a6a
£Dev <djefer09@gmail.com>
2026-01-25 19:38:18 +0200

    my own coomit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        PrintReceipt();
+    }
+        static void PrintReceipt()
+        {
+            PrintHeader();
+            PrintBody();
+            PrintFooter();
+        }
+        static void PrintHeader()
+        {
+            System.Console.WriteLine("Cash Receipt");
+            System.Console.WriteLine("------------------------------");
+        }
+        static void PrintBody()
+        {
+            System.Console.WriteLine("Charged to____________________");
+            System.Console.WriteLine("Received by___________________");
+        }
+        static void PrintFooter()
+        {
+            System.Console.WriteLine("------------------------------");
+            System.Console.WriteLine("\u00A9 IT KARIERA");
+        }
+}
